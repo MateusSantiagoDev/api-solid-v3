05deb33c8d92c67be325ab514f5ba6fdcb4296f6
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-02 20:01:48 -0300

    feat: Get User Profile Use Case

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,26 @@
+import { hash } from 'bcryptjs'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('Should be able to get user profile', async () => {
+    const createUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123123', 6),
+    })
+    const { user } = await sut.execute({
+      userId: createUser.id,
+    })
+    expect(user.name).toEqual('John Doe')
+  })
+})
---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,24 @@
+import { User } from '@prisma/client'
+import { UsersRepository } from '@/repositories/users-repository'
+
+interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId)
+    if (!user) {
+      throw new Error()
+    }
+    return { user }
+  }
+}
