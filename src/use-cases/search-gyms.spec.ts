2b1713dbd62addcc2cc43ae27818bef78713973f
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-03 11:46:10 -0300

    feat: Should be able to fetch paginated gyms search

--- src/use-cases/search-gyms.spec.ts
+++ src/use-cases/search-gyms.spec.ts
@@ -33,4 +33,25 @@ describe('Search Gyms Use Case', () => {
     expect(gyms).toHaveLength(1)
     expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
   })
+
+  it('Should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `JavaScript Gym ${i}`,
+        description: null,
+        phone: null,
+        latitude: -23.7137545,
+        longitude: -46.6995227,
+      })
+    }
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 2,
+    })
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JavaScript Gym 21' }),
+      expect.objectContaining({ title: 'JavaScript Gym 22' }),
+    ])
+  })
 })
